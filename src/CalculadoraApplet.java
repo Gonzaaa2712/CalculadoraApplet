472e297d937baf01dd764f98fb76a46753476e58
gonzaaa2712 <gonzalo.ramiro27@gmail.com>
2021-09-28 14:17:24 +0200

    Modificada clase CalculadoraApplet

--- src/CalculadoraApplet.java
+++ src/CalculadoraApplet.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
 import java.applet.Applet;
+import java.awt.*;
 
 public class CalculadoraApplet extends JApplet {
-    private InterfazCalculadora ventanaCalculadora;
+    private InterfazCalculadora ventanaCalculadora = new InterfazCalculadora();
+
+    public void init(){
+        this.setLayout(new BorderLayout());
+        add(ventanaCalculadora, BorderLayout.CENTER);
+
+
+    }
 }
